8b2a03cf5a0b49e22249693e260cccdcfd332c40
shenzhanbo <szbltyy@hotmail.com>
2019-10-05 17:05:55 +0800

    modified README.md

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# TencentVideoD
-Download the video from Tencent (v.qq.com)
+## Tencent Video Downloader 
+This is a project that could download the free or vip video from [Tencent video]("http://v.qq.com"). This just a little project for fun, please do not use it in commerce。
+
+#### 1. lib
+* **urllib.request**: to simulate the request
+* **urllib.parse.quote**: transform the chinese character to utf-8 char
+* **bs4.BeautifulSoup**: to extract the source from html
+* **selenium.webdriver**: to simulate the action of web browser
+* **selenium.webdriver.ActionChains**: to simulate the action of mouse
+* **import win32clipboard**: to get the conten from clipboard
+* **pyautogui**: click the browser
+
+#### 2. the class: API
+This class is used to get the API, only have these API can you download the vip video
+
+#### 3. the class: crawler
+*I define a class named Spider to crawl the video and download them*
+
+1. **\_\_init\_\_(self, keyword)**
+   > Initate the crawler, get the keyword, and change it to utf-8 code and save the keyword and the search url by splicing the api of tencent
+
+2. **Get_list(self)**
+   > To get the links of video from display album. **This part can not crawl the correct link from different page. So when you want to crawl the video, you need to inspect the label of html, and rewrite this part so that you can get the correct link from page**
+
+3. **vip_Downloader(self)**
+   > Use API download the vip video using selenium. Different videos have different result by using different API. **So if it doesn't work by using one, you need to change to another.** And API can be got from the API.py
+
+#### 4. Shortage
+1. You have to rewrite the function Get_list() when you want to download different video
+2. You have to wait and do not move the mouse out of the browser which will get a wrong reslut
+3. Didn't change the API automatically, you have to try which one can download the vip video.
+
+#### 5. About
+If you have any questions, email me in szbltyy@hotmail.com
--- crawler.py
+++ crawler.py
@@ -16,11 +16,14 @@ from urllib.parse import quote
 import string
 from bs4 import BeautifulSoup
 import json
-from selenium import webdriver
-from selenium.webdriver import ActionChains
-import time
+from selenium import webdriver  # used to simulate the action of web browser
+from selenium.webdriver import ActionChains  # used to simulate the action of mouse
+import time  # used to get a delay
+from selenium.webdriver.common.keys import Keys  # used to simulate the action of keyboard
+import win32clipboard as wc  # used to get the conten from clipboard
+import win32con
 import pyautogui
-import pyperclip
+import os
 
 
 class Spider:
@@ -31,17 +34,22 @@ class Spider:
     """
 
     # Init the information of crawler
-    def __init__(self, url):
-        self.name = "Zackaree"
+    def __init__(self, keyword):
         # urllib.request.urlopen() do not support the url both have English and Chinese
         # So we should use urllib.parse.quote to split the url
         # That we make sure urllib could open the url correctly
+        url = 'http://v.qq.com/x/search/?q=' + keyword
         url_s = quote(url, safe=string.printable)
+        self.keyword = keyword
         self.url = url_s
 
     def Get_list(self):
         """
         Get the video list from html
+        !!!Remember:
+        This part can not crawl the correct link from different page
+        So when you want to crawl the video, you need to inspect the label of html
+        and rewrite this part so that you can get the correct link from page
         :return:
         """
         # get the html by keyword
@@ -65,21 +73,26 @@ class Spider:
         for list in play_soup.find_all('a', attrs={'_stat': "videolist:click"})[5:]:
             self.links.append('https://v.qq.com' + list['href'])
 
+    # ==========================================================================================================
+    # This functon is used to download the free video from tencent
+    # But it's only support download the 270p and 480p
     def free_Downloader(self):
         """
-        Download the video
+        Download the video using urllib.request and BeautifulSoup
         # @ http://vv.video.qq.com/getinfo : the API that get 270p json from v.qq.com
         # @ http://vv.video.qq.com/getkey  : the API that get 480p json from v.qq.com
         :return:
         """
-        self.Get_list()
 
+        self.Get_list()
+        root = "D:\\" + self.keyword + '\\'
+        if os.path.exists(root) is False:
+            os.mkdir(root)
+        num = 1
 
         for link in self.links:
             # Split the id of video
             vid = ((link.split('.', 5)[-2]).split('/'))[-1]
-
-            # ==================================================================================================
             get_json = request.urlopen("http://vv.video.qq.com/getinfo?vids=" + vid +
                                        "&platform=101001&charge=0&otype=json&defn=shd")
             get_json = get_json.read().decode('utf-8')[len('QZOutputJson='): -1]
@@ -98,48 +111,80 @@ class Spider:
             v_480p_url = tempStr['vl']['vi'][0]['ul']['ui'][0]['url'] + filename + '?vkey=' + tempStr2['key']
 
             res = request.urlopen(v_480p_url).read()
-            root = "D:\\"
-            print("Downloading: " + root + filename + ".....")
+            files = self.keyword + '第' + str(num) + '集.mp4'
+            num += 1
+            print("Downloading: " + root + files + ".....")
             with open(root + filename, 'wb') as f:
                 f.write(res)
             print("Download completed!")
 
+        print('All videos of the album have been downloaded!')
+
+    # ==========================================================================================================
+
     def vip_Downloader(self):
         """
-        # @ https://play.fo97.cn/?url= : 全网vip
-        # @ http://www.guandianzhiku.com/v/s/?url= : 智库解析
-        # @ https://www.ckmov.vip/api.php?url= : 解析系统
+        Use API download the vip video using selenium
+        !!!Remember:
+        Different videos have different result by using different API
+        So if it doesn't work by using one
+        you need to change to another
+        API can be got from the API.py
         :return:
 
         """
-        self.Get_list()
 
-        link = self.links[1]
-        # This part can download the vip video from v.qq.com
-        url = 'https://play.fo97.cn/?url=' + link
-        browser = webdriver.Chrome(executable_path='./chromedriver')
-        browser.get(url)
-        time.sleep(30)
-        display = browser.find_element_by_xpath("//iframe")
-        rightclick = ActionChains(browser)
-        rightclick.context_click(display).perform()
-        time.sleep(5)
-        pyautogui.typewrite(['down', 'down', 'down', 'down'])
-        time.sleep(5)
+        self.Get_list()
+        root = "D:\\" + self.keyword + '\\'
+        if os.path.exists(root) is False:
+            os.mkdir(root)
+        num = 1
+
+        for link in self.links:
+            url = 'http://yun.360dy.wang/jx.php?url=' + link
+            # use webdriver simulate the action of chrome
+            browser = webdriver.Chrome(executable_path='./chromedriver.exe')
+            browser.get(url)
+            time.sleep(10)  # wait the url loading
+            right_click = browser.find_element_by_tag_name('div')
+            # ActionChain could simulate the action of mouse and keyboard
+            rightclick = ActionChains(browser)
+
+            # double right click, get the menu
+            rightclick.context_click(right_click)
+            rightclick.context_click(right_click).perform()
+            time.sleep(1)
+
+            # choose the "copy video address..." option
+            pyautogui.typewrite(['down', 'down', 'down', 'down', 'down'])
+            time.sleep(1)
+            # it must write the "enter", else the choice can't be chosen
+            pyautogui.typewrite(['enter'])
+            time.sleep(1)
+            video_url = str(self.get_clipboard())[2:]
+
+            res = request.urlopen(video_url).read()
+            filename = self.keyword + '第' + str(num) + '集.mp4'
+            print("Downloading: " + root + filename + ".....")
+            with open(root + filename, 'wb') as f:
+                f.write(res)
+            print("Download completed!")
+            num += 1
+            time.sleep(5)
         browser.close()
         browser.quit()
-
-        # for list in vip_soup.find_all('video'):
-        #    print(list)
 
     def get_clipboard(self):
         """
         To get the content of clipboard
+        so we can get the real url of video
         :return:
         """
-
-
+        wc.OpenClipboard()
+        url = wc.GetClipboardData(win32con.CF_TEXT)
+        wc.CloseClipboard()  # it must be close, else we will get the same url from clipboard
+        return url
 
 if __name__ == '__main__':
-    spider = Spider('http://v.qq.com/x/search/?q=假面骑士创骑')
+    spider = Spider('假面骑士创骑')
     spider.vip_Downloader()
